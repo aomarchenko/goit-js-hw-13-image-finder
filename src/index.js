dc315e22ff4f876c848a6ea0189a45a54ebf82fe
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-10-03 16:50:43 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import './sass/main.scss';
-import fetchImages from './js/apiService';
+import ApiService from './js/apiService';
+import imageCardTemplate from './templates/image-card-template.hbs';
 
-fetchImages();
+const refs = {
+  searchForm: document.querySelector('.input-form'),
+  loadMoreButton: document.querySelector('.load-more-button'),
+  imageContainer: document.querySelector('.image-container'),
+};
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreButton.addEventListener('click', onLoadMore);
+
+const apiService = new ApiService();
+
+function onSearch(e) {
+  e.preventDefault();
+  apiService.query = e.currentTarget.elements.query.value;
+  apiService.resetPage();
+  apiService.fetchImages().then(appendImageGridMarkup);
+}
+function onLoadMore() {
+  apiService.fetchImages().then(appendImageGridMarkup);
+}
+function appendImageGridMarkup(data) {
+  refs.imageContainer.insertAdjacentHTML('beforeend', imageCardTemplate(data));
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,7 +1,35 @@
-export default function fetchImages(query) {
-  return fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=5&key=23676314-92d729b6642f8dfd3ee72d5a9`,
-  ).then(response => {
-    return response.json().then(console.log);
-  });
+// export default function fetchImages(searchQuery) {
+//   return fetch(
+//     `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=5&key=23676314-92d729b6642f8dfd3ee72d5a9`,
+//   ).then(response => {
+//     return response.json().then(console.log);
+//   });
+// }
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchImages(searchQuery) {
+    console.log(this);
+    return fetch(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=5&key=23676314-92d729b6642f8dfd3ee72d5a9`,
+    ).then(response => {
+      return response.json().then(data => {
+        this.page += 1;
+        console.log(data);
+        return data.this;
+      });
+    });
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+  resetPage() {
+    this.page = 1;
+  }
 }
