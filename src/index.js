d5a096ce585912bdf5d447ef3148e8c1157b92df
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2021-10-03 18:11:38 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const refs = {
   loadMoreButton: document.querySelector('.load-more-button'),
   imageContainer: document.querySelector('.image-container'),
 };
+const element = refs.imageContainer;
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreButton.addEventListener('click', onLoadMore);
@@ -15,14 +16,27 @@ const apiService = new ApiService();
 
 function onSearch(e) {
   e.preventDefault();
+  clearImageContainer();
   apiService.query = e.currentTarget.elements.query.value;
   apiService.resetPage();
   apiService.fetchImages().then(appendImageGridMarkup);
+
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
 function onLoadMore() {
   apiService.fetchImages().then(appendImageGridMarkup);
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
 function appendImageGridMarkup(data) {
   console.log(data);
   refs.imageContainer.insertAdjacentHTML('beforeend', imageCardTemplate(data));
+}
+function clearImageContainer() {
+  refs.imageContainer.innerHTML = '';
 }
